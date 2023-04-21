b277c18e7f0a132bddc8662223419b1886c871cf
raalonzo <raalonzo@uamv.edu.ni>
2023-04-21 10:23:31 -0600

    new commit

--- SteamReviews/models/example/app.sql
+++ SteamReviews/models/example/app.sql
@@ -1,8 +1,12 @@
 {{ config(materialized='table') }}
 
+with {{ref('steam_reviews')}} as (
 
+    select app_id as app_id
+    union all
+    select app_name as app_name
 
-select 
-    app_id as app_id
-    app_name as app_name
+)
+
+select *
 from {{ref('steam_reviews')}}
