80b32dabb213132f3cc75e806327936f4fbc428e
raalonzo <raalonzo@uamv.edu.ni>
2023-06-05 18:36:15 -0600

    New Commit

--- SteamReviews/models/example/acquisition.sql
+++ SteamReviews/models/example/acquisition.sql
@@ -1,5 +1,5 @@
 with acquisition_info as (
-    select acquisition_id, steam_purchase, steam_gifted from {{ref('steam_reviews')}}
+    select acquisition_id, steam_purchase, steam_gifted from {{ref('steam_reviews_clean')}}
 )
 
 select * from acquisition_info
